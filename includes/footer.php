aab68a4358d2d2a0f17844681edd33dc41ca8653
Tan Zhi Shan <70124100+YikRyu@users.noreply.github.com>
2022-10-14 00:25:25 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,6 +3,6 @@
       <div class="pull-right hidden-xs">
         <b>All rights reserved</b>
       </div>
-      <strong>Copyright &copy; 2018 Brought to You By Group 1 Praga and the Gang</strong>
+      <strong>Copyright &copy; 2022 Brought to You By Group 1 Praga and the Gang</strong>
     </div>
 </footer>
