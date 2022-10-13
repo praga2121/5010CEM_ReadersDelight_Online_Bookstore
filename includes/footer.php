ba817d1ddbe094358d047d7ea4faf91b7d87c9a8
Tan Zhi Shan <70124100+YikRyu@users.noreply.github.com>
2022-10-14 00:24:53 +0800

    update new footer content

--- includes/footer.php
+++ includes/footer.php
@@ -3,6 +3,6 @@
       <div class="pull-right hidden-xs">
         <b>All rights reserved</b>
       </div>
-      <strong>Copyright &copy; 2018 Brought to You By <a href="https://freeprojectscodes.com/">freeprojectscodes</a></strong>
+      <strong>Copyright &copy; 2018 Brought to You By Group 1 Praga and the Gang</strong>
     </div>
 </footer>
