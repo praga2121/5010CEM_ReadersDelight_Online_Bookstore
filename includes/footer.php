e3729766404d79dacb3052f69084ef9ece3c21f1
Mashiro <vpragatheswaran@gmail.com>
2022-10-14 12:47:00 +0800

    Contact Page and Updated SQL.
    Replaced files and change the DB to latest (my one), do export on your phpmyadmin and update whatever you do at your machine. (This commit has faulty for registration and sign up). Other modules are fine).

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b></b>
+      </div>
+      <strong>Copyright &copy; 2022 Brought to You By Group 1 Praga and the Gang.</strong>
+    </div>
+</footer>
---
+++ login.php
@@ -0,0 +1,62 @@
+<!--script file for successfully logged in alerts and page redirecting-->
+<script type="text/javascript">
+    function loginAlerts() {
+		alert("Login successful! Redirecting home page....");
+        window.location = "cart_view.php"; // Redirecting to other page.
+    }
+</script>
+
+<?php include 'includes/session.php'; ?>
+<?php
+  if(isset($_SESSION['user'])){
+    echo '<script type="text/javascript">loginAlerts()</script>';
+  }
+?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition login-page">
+<div class="login-box">
+  	<?php
+      if(isset($_SESSION['error'])){
+        echo "
+          <div class='callout callout-danger text-center'>
+            <p>".$_SESSION['error']."</p> 
+          </div>
+        ";
+        unset($_SESSION['error']);
+      }
+      if(isset($_SESSION['success'])){
+        echo "
+          <div class='callout callout-success text-center'>
+            <p>".$_SESSION['success']."</p> 
+          </div>
+        ";
+        unset($_SESSION['success']);
+      }
+    ?>
+  	<div class="login-box-body">
+    	<p class="login-box-msg">ACCOUNT SIGN IN</p>
+
+    	<form action="verify.php" method="POST">
+      		<div class="form-group has-feedback">
+        		<input type="text" class="form-control" name="email" placeholder="Email" required>
+        		<span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      		</div>
+          <div class="form-group has-feedback">
+            <input type="password" class="form-control" name="password" placeholder="Password" required>
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+          </div>
+      		<div class="row">
+    			<div class="col-xs-4">
+          			<button type="submit" class="btn btn-primary btn-block btn-flat" name="login"><i class="fa fa-sign-in"></i> Sign In</button>
+        		</div>
+      		</div>
+    	</form>
+      <br>
+      <a href="signup.php" class="text-center">Register a new account</a><br>
+      <a href="index.php"><i class="fa fa-home"></i> Back to Home Page</a>
+  	</div>
+</div>
+	
+<?php include 'includes/scripts.php' ?>
+</body>
+</html>
