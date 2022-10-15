9c6ebc7b6f0405bffad04c1794f0e1b9eb1e1d00
IAmDanieI <mypratastastegood@gmail.com>
2022-10-15 21:14:26 +0800

    Chat Bot

---
+++ chatbot/js/responses.js
@@ -0,0 +1,21 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else if (input == "Praga gay") {
+        return "I Agree!";
+    }else {
+        return "Try asking something else!";
+    }
+}
--- index.php
+++ index.php
@@ -2,7 +2,56 @@
 <?php include 'includes/header.php'; ?>
 <body class="hold-transition skin-blue layout-top-nav">
 <div class="wrapper">
+<!-- CHAT BAR BLOCK -->
+<link rel="stylesheet" href="chatbot/css/chat.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<div class="chat-bar-collapsible">
+        <button id="chat-button" type="button" class="collapsible">Chat with us!
+            <i id="chat-icon" style="color: #fff;" class="fa fa-fw fa-comments-o"></i>
+        </button>
 
+        <div class="chat-content">
+            <div class="full-chat-block">
+                <!-- Message Container -->
+                <div class="outer-container">
+                    <div class="chat-container">
+                        <!-- Messages -->
+                        <div id="chatbox">
+                            <h5 id="chat-timestamp"></h5>
+                            <p id="botStarterMessage" class="botText"><span>Loading...</span></p>
+                        </div>
+
+                        <!-- User input box -->
+                        <div class="chat-bar-input-block">
+                            <div id="userInput">
+                                <input id="textInput" class="input-box" type="text" name="msg"
+                                    placeholder="Tap 'Enter' to send a message">
+                                <p></p>
+                            </div>
+
+                            <div class="chat-bar-icons">
+                                <i id="chat-icon" style="color: crimson;" class="fa fa-fw fa-heart"
+                                    onclick="heartButton()"></i>
+                                <i id="chat-icon" style="color: #333;" class="fa fa-fw fa-send"
+                                    onclick="sendButton()"></i>
+                            </div>
+                        </div>
+
+                        <div id="chat-bar-bottom">
+                            <p></p>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="chatbot/js/responses.js"></script>
+<script src="chatbot/js/chat.js"></script>
+	<!-- //end of bot -->
 	<?php include 'includes/navbar.php'; ?>
 	 
 	  <div class="content-wrapper">
@@ -96,8 +145,7 @@
 	      </section>
 	     
 	    </div>
-	  </div>
-  
+	  </div>  
   	<?php include 'includes/footer.php'; ?>
 </div>
 
