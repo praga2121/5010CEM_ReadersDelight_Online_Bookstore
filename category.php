1c300938ba59cbf8a0fd47544f36236f3cdbdba1
Tan Zhi Shan <70124100+YikRyu@users.noreply.github.com>
2022-10-16 02:27:09 +0800

    new UI and added search by author

--- category.php
+++ category.php
@@ -30,44 +30,49 @@
 	      <section class="content">
 	        <div class="row">
 	        	<div class="col-sm-9">
-		            <h1 class="page-header"><?php echo $cat['name']; ?></h1>
-		       		<?php
-		       			
-		       			$conn = $pdo->open();
+					<div class="box box-solid">
+						<div class="box-body">
+		            		<h1 class="page-header"><?php echo $cat['name']; ?></h1>
+							<?php
+								
+								$conn = $pdo->open();
 
-		       			try{
-		       			 	$inc = 3;	
-						    $stmt = $conn->prepare("SELECT * FROM products WHERE category_id = :catid");
-						    $stmt->execute(['catid' => $catid]);
-						    foreach ($stmt as $row) {
-						    	$image = (!empty($row['photo'])) ? 'images/'.$row['photo'] : 'images/noimage.jpg';
-						    	$inc = ($inc == 3) ? 1 : $inc + 1;
-	       						if($inc == 1) echo "<div class='row'>";
-	       						echo "
-	       							<div class='col-sm-4'>
-	       								<div class='box box-solid'>
-		       								<div class='box-body prod-body'>
-		       									<img src='".$image."' width='100%' height='230px' class='thumbnail'>
-		       									<h5><a href='product.php?product=".$row['slug']."'>".$row['name']."</a></h5>
-		       								</div>
-		       								<div class='box-footer'>
-		       									<b>&#36; ".number_format($row['price'], 2)."</b>
-		       								</div>
-	       								</div>
-	       							</div>
-	       						";
-	       						if($inc == 3) echo "</div>";
-						    }
-						    if($inc == 1) echo "<div class='col-sm-4'></div><div class='col-sm-4'></div></div>"; 
-							if($inc == 2) echo "<div class='col-sm-4'></div></div>";
-						}
-						catch(PDOException $e){
-							echo "There is some problem in connection: " . $e->getMessage();
-						}
+								try{
+									$inc = 3;	
+									$stmt = $conn->prepare("SELECT * FROM products WHERE category_id = :catid");
+									$stmt->execute(['catid' => $catid]);
+									foreach ($stmt as $row) {
+										$image = (!empty($row['photo'])) ? 'images/'.$row['photo'] : 'images/noimage.jpg';
+										$inc = ($inc == 3) ? 1 : $inc + 1;
+										if($inc == 1) echo "<div class='row'>";
+										echo "
+											<div class='col-sm-4'>
+												<div class='box box-solid'>
+													<div class='box-body prod-body'>
+														<img src='".$image."' width='100%' height='230px' class='thumbnail'>
+														<h5 class='prod-text'><a href='product.php?product=".$row['slug']."'>".$row['name']."</a></h5>
+														<h5 class='prod-text'>".$row['author']."</h5>
+													</div>
+													<div class='box-footer'>
+														<b>&#36; ".number_format($row['price'], 2)."</b>
+													</div>
+												</div>
+											</div>
+										";
+										if($inc == 3) echo "</div>";
+									}
+									if($inc == 1) echo "<div class='col-sm-4'></div><div class='col-sm-4'></div></div>"; 
+									if($inc == 2) echo "<div class='col-sm-4'></div></div>";
+								}
+								catch(PDOException $e){
+									echo "There is some problem in connection: " . $e->getMessage();
+								}
 
-						$pdo->close();
+								$pdo->close();
 
-		       		?> 
+							?> 
+						</div>
+					</div>
 	        	</div>
 	        	<div class="col-sm-3">
 	        		<?php include 'includes/sidebar.php'; ?>
