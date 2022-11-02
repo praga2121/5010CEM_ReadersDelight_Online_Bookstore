05814e5b73f3c68d48b60e2cd4e8ca5faf3bffbf
IAmDanieI <mypratastastegood@gmail.com>
2022-11-02 23:38:28 +0800

    Added limit based on stock

--- product.php
+++ product.php
@@ -61,11 +61,12 @@
 									<br><br>
 									<form class="form-inline" id="productForm">
 										<div class="form-group">
-											<div class="input-group col-sm-5">
+											<div class="input-group col-sm-6">
 												<span class="input-group-btn">
 													<button type="button" id="minus" class="btn btn-default btn-flat btn-lg"><i class="fa fa-minus"></i></button>
 												</span>
-												<input type="text" name="quantity" id="quantity" class="form-control input-lg" value="1">
+												<?php $maximum = $product['stock']; ?>
+												<input type="number" name="quantity" id="quantity" class="form-control input-lg" value="1" min="1" max="<?php echo $maximum?>">
 												<span class="input-group-btn">
 													<button type="button" id="add" class="btn btn-default btn-flat btn-lg"><i class="fa fa-plus"></i>
 													</button>
@@ -74,6 +75,7 @@
 											</div>
 											<button type="submit" class="btn btn-primary btn-lg btn-flat"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
 										</div>
+										<h3><b>Current Stock: <?php echo ($product['stock']); ?></b></h3>
 									</form>
 								</div>
 								<div class="col-sm-6">
