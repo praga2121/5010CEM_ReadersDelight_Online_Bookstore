9c9a5343a17eb4002905f89620aec02719b7321c
Tan Zhi Shan <70124100+YikRyu@users.noreply.github.com>
2022-11-07 00:53:41 +0800

    add shipping tab

--- admin/includes/navbar.php
+++ admin/includes/navbar.php
@@ -2,7 +2,7 @@
   <!-- Logo -->
   <a href="#" class="logo">
     <!-- mini logo for sidebar mini 50x50 pixels -->
-    <span class="logo-mini"><b>C</b>P</span>
+    <span class="logo-mini"><b>R</b>D</span>
     <!-- logo for regular state and mobile devices -->
     <span class="logo-lg"><b>readersDelight</b>Site</span>
   </a>
