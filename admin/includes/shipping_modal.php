f8e41e0068d319249949118f1af83fd7cb1b0dce
Tan Zhi Shan <70124100+YikRyu@users.noreply.github.com>
2022-11-09 00:21:43 +0800

    shipping, not doneeeeeeeeeeeee

--- admin/includes/shipping_modal.php
+++ admin/includes/shipping_modal.php
@@ -12,8 +12,8 @@
               <p>Transaction ID: <span id="transid"></span></p>
               <p>Expected Delivery Date: <span id="ship_date"></span></p>
               <p>Delivery Status: <span style="" id="ship_status"></span></p>
-              <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.1.min.js"> //for coloring the status text
-                $(document).ready(function(){
+              <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.1.min.js"> 
+                $(document).ready(function(){ //change status color
                   var ship_status = $(#ship_status).data();
                   if(ship_status === "Preparing for Shipment"){
                     $(#ship_status).attr("style", "color:#808080;");
@@ -52,3 +52,52 @@
         </div>
     </div>
 </div>
+
+<!-- Edit Shipping Date and Status-->
+<div class="modal fade" id="edit">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title"><b>Edit Shipping Status</b></h4>
+            </div>
+            <div class="modal-body">
+              <form class="form-horizontal" method="POST" action="shipping_edit.php">
+                <input type="hidden" id="sales_id" name="sales_id">
+                <div class="form-group">
+                  <div class="col-sm-5">
+                    <p>Transaction Date: <span id="date"></span></p>
+                    <p>Transaction ID: <span id="transid"></span></p>
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label for="ship_date" class="col-sm-1 control-label">Expected Delivery Date</label>
+
+                  <div class="col-sm-5">
+                    <input type="date" class="form-control pull-right col-sm-8" id="ship_date" name="ship_date">
+                  </div>
+
+                  <label for="ship_status" class="col-sm-1 control-label">Shipment Status</label>
+
+                  <div class="col-sm-5">
+                    <select class="form-control" name="ship_status">
+                      <option value="0">Preparing for Shipment</option>
+                      <option value="1">On the Way</option>
+                      <option value="2">Delivered</option>
+                      <option value="3">Delayed</option>
+                    </select>
+                  </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default btn-flat pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Close</button>
+              <button type="submit" class="btn btn-success btn-flat" name="edit"><i class="fa fa-check-square-o"></i> Update</button>
+              </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+</script>
