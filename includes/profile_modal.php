cbb98a28ad58145521a4d81a3b39e85f0fa0a42f
Tan Zhi Shan <70124100+YikRyu@users.noreply.github.com>
2022-11-15 17:26:14 +0800

    shipping user

--- includes/profile_modal.php
+++ includes/profile_modal.php
@@ -12,23 +12,6 @@
               <p>Transaction ID: <span id="transid"></span></p>
               <p>Expected Delivery Date: <span id="ship_date"></span></p>
               <p>Delivery Status: <span style="" id="ship_status"></span></p>
-              <script type="text/javascript" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.1.min.js"> //for coloring the status text
-                $(document).ready(function(){
-                  var ship_status = $(#ship_status).data();
-                  if(ship_status === "Preparing for Shipment"){
-                    $(#ship_status).attr("style", "color:#808080;");
-                  }
-                  else if(ship_status ==="Shipment On the Way"){
-                    $(#ship_status).attr("style", "color:#000000;");
-                  }
-                  else if(ship_status ==="Delivered"){
-                    $(#ship_status).attr("style", "color:#00FF00;");
-                  }
-                  else{
-                    $(#ship_status).attr("style", "color:#FF0000;");
-                  }
-                });
-              </script>
 
               <table class="table table-bordered">
                 <thead>
