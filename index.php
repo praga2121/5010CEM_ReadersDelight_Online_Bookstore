39244302cf2272e4f59960e1f1b694b472ef8812
Theshan 08 <theshanraj13@gmail.com>
2022-11-19 15:53:41 +0800

    little info

--- index.php
+++ index.php
@@ -141,7 +141,8 @@
 
 									try{
 										$inc = 3;	
-										$stmt = $conn->prepare("SELECT *, SUM(quantity) AS total_qty FROM details LEFT JOIN sales ON sales.id=details.sales_id LEFT JOIN products ON products.id=details.product_id WHERE MONTH(sales_date) = '$month' GROUP BY details.product_id ORDER BY total_qty DESC LIMIT 3");
+										$stmt = $conn->prepare("SELECT *, SUM(quantity) AS total_qty FROM details LEFT JOIN sales ON sales.id=details.sales_id LEFT JOIN products 
+										ON products.id=details.product_id WHERE MONTH(sales_date) = '$month' GROUP BY details.product_id ORDER BY total_qty DESC LIMIT 3");
 										$stmt->execute();
 										foreach ($stmt as $row) {
 											$image = (!empty($row['photo'])) ? 'images/'.$row['photo'] : 'images/noimage.jpg';
